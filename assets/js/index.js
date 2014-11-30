5564244514379789c6715d67c26316eea412a437
changdrew <changdrew@berkeley.edu>
2014-11-30 02:35:52 -0800

    first panic
    awyisss

---
+++ assets/js/index.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+	var fourteen_east = ["IND", "MIA", "TOR", "CHI", "WAS", "NJN", "CHA", "ATL", "NYK", "CLE", "DET", "BOS", "ORL", "PHI", "MIL"];
+	var fourteen_west = 
+	var thirteen_east = 
+	var thirteen_west = 
+	var twelve_east = 
+	var twelve_west = 
+	var eleven_east = 
+	var eleven_west = 
+	var ten_east = 
+	var ten_west = 
+	var nine_east = 
+	var nin
+
+	$("#header-container").click(function(){
+
+	});
+
+
+}
---
+++ assets/js/script.js
@@ -0,0 +1,41 @@
+$(document).ready(function() {
+	var fourteen_east = ["IND", "MIA", "TOR", "CHI", "WAS", "BRK", "CHA", "ATL", "NYK", "CLE", "DET", "BOS", "ORL", "PHI", "MIL"];
+	var fourteen_west = ["SAS", "OKC", "LAC", "HOU", "POR", "GSW", "MEM", "DAL", "PHO", "MIN", "DEN", "NOP", "SAC", "LAL", "UTA"];
+	var fourteen_champs = "SAS"
+	var thirteen_east = ["MIA", "NYK", "IND", "BRK", "CHI", "ATL", "BOS", "MIL", "PHI", "TOR", "DET", "WAS", "CLE", "CHA", "ORL"]
+	var thirteen_west = ["OKC", "SAS", "DEN", "LAC", "MEM", 'GSW', 'LAL', 'HOU', 'UTA', 'DAL', 'POR', 'MIN', 'SAC', 'NOH', 'PHO']
+	var thirteen_champs = "MIA"
+	var twelve_east = ['CHI', 'MIA', 'IND', 'BOS', 'ATL', 'ORL', 'NYK', 'PHI', 'MIL', 'DET', 'TOR', 'NJN', 'CLE', 'WAS', 'CHA']
+	var twelve_west = ['SAS', 'OKC', 'LAL', 'MEM', 'LAC', 'DEN', 'DAL', 'UTA', 'HOU', 'PHO', 'POR', 'MIN', 'GSW', 'SAC', 'NOH']
+	var twelve_champs = "MIA"
+	var eleven_east = ["CHI", 'MIA', 'BOS', 'ORL', 'ATL', 'NYK', 'PHI', 'IND', 'MIL', 'CHA', 'DET', 'NJN', 'WAS', 'TOR', 'CLE']
+	var eleven_west = ['SAS', 'LAL', 'DAL', 'OKC', 'DEN', 'POR', 'NOH', 'MEM', 'HOU', 'PHO', 'UTA', 'GSW', 'LAC', 'SAC', 'MIN']
+	var eleven_champs = "DAL"
+	var ten_east = ['CLE', 'ORL', 'ATL', 'BOS', 'MIA', 'MIL', 'CHA', 'CHI', 'TOR', 'IND', 'NYK', 'DET', 'PHI', 'WAS', 'NJN']
+	var ten_west = ['LAL', 'DAL', 'PHO', 'DEN', 'UTA', 'POR', 'SAS', 'OKC', 'HOU', 'MEM', 'NOH', 'LAC', 'GSW', 'SAC', 'MIN']
+	var ten_champs = "LAL"
+	var nine_east = ['CLE', 'BOS', 'ORL', 'ATL', 'MIA', 'PHI', 'CHI', 'DET', 'IND', 'CHA', 'NJN', 'MIL', 'TOR', 'NYK', 'WAS']
+	var nine_west = ['LAL', 'DEN', 'SAS', 'POR', 'HOU', 'DAL', 'NOH', 'UTA', 'PHO', 'GSW', 'MIN', 'MEM', 'OKC', 'LAC', 'SAC']
+	var nine_champs = 'LAL'
+	var eight_east = []
+	var eight_west = []
+	var eight_champs = 
+	var seven_east = []
+	var seven_west = []
+	var seven_champs = 
+	var six_east = []
+	var six_west = []
+	var six_champs = 
+	var five_east = []
+	var five_west = []
+	var five_champs = 
+	var four_east = []
+	var four_west = []
+	var four_champs = 
+
+	$("#header-container").click(function(){
+
+	});
+
+
+}
